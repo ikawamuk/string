1dafc437085c7a5cfa39314cbe9100b2125c28af
ikawamuk <ikawamuk@student.42.fr>
2026-09-08 09:19:07 +0900

    var name

--- String.c
+++ String.c
@@ -19,35 +19,35 @@ int     string_construct(String *s, const char *str)
 {
     if (!s) return (-1);
     if (!str) str = "";
-    String_impl_    *self = (String_impl_*)s;
-    self->length = strlen(str);
-    self->capacity = grow_capacity_(self->length);
-    self->data = calloc(self->capacity, sizeof(char));
-    if (!self->data) return (-1);
-    strcpy(self->data, str);
+    String_impl_    *this = (String_impl_*)s;
+    this->length = strlen(str);
+    this->capacity = grow_capacity_(this->length);
+    this->data = calloc(this->capacity, sizeof(char));
+    if (!this->data) return (-1);
+    strcpy(this->data, str);
     return (0);
 }
 
 void    string_destruct(String *s)
 {
     if (!s) return ;
-    String_impl_    *self = (String_impl_*)s;
-    free(self->data);
-    self->data = NULL;
-    self->length = 0;
-    self->capacity = 0;
+    String_impl_    *this = (String_impl_*)s;
+    free(this->data);
+    this->data = NULL;
+    this->length = 0;
+    this->capacity = 0;
 }
 
 int string_copy_construct(String *s, const String *oth)
 {
     if (!s || !oth) return (-1);
-    String_impl_    *self = (String_impl_*)s;
+    String_impl_    *this = (String_impl_*)s;
     const String_impl_    *other = (const String_impl_*)oth;
-    self->length = other->length;
-    self->capacity = other->capacity;
-    self->data = calloc(self->capacity, sizeof(char));
-    if (!self->data) return (-1);
-    strcpy(self->data, other->data);
+    this->length = other->length;
+    this->capacity = other->capacity;
+    this->data = calloc(this->capacity, sizeof(char));
+    if (!this->data) return (-1);
+    strcpy(this->data, other->data);
     return (0);
 }
 
@@ -55,19 +55,19 @@ String  *string_assign(String *l, const String *r)
 {
     if (!l || !r) return (NULL);
     if (l != r) {
-        String_impl_        *self  = (String_impl_ *)l;
+        String_impl_        *this  = (String_impl_ *)l;
         const String_impl_  *other = (const String_impl_ *)r;
-        if (self->capacity < other->length + 1)
+        if (this->capacity < other->length + 1)
         {
             size_t  new_cap = grow_capacity_(other->length);
             char    *tmp = calloc(new_cap, sizeof(char));
             if (!tmp) return (NULL);
-            free(self->data);
-            self->data = tmp;
-            self->capacity = new_cap;
+            free(this->data);
+            this->data = tmp;
+            this->capacity = new_cap;
         }
-        memcpy(self->data, other->data, other->length + 1);
-        self->length = other->length;
+        memcpy(this->data, other->data, other->length + 1);
+        this->length = other->length;
     }
     return (l);
 }
@@ -75,41 +75,41 @@ String  *string_assign(String *l, const String *r)
 char    *string_begin(String *s)
 {
     if (!s) return (NULL);
-    const String_impl_    *self = (const String_impl_*)s;
-    return (self->data);
+    const String_impl_    *this = (const String_impl_*)s;
+    return (this->data);
 }
 
 char    *string_end(String *s)
 {
     if (!s) return (NULL);
-    const String_impl_    *self = (const String_impl_*)s;
-    return (self->data + self->length);
+    const String_impl_    *this = (const String_impl_*)s;
+    return (this->data + this->length);
 }
 
 
 size_t  string_length(const String *s)
 {
     if (!s) return (-1);
-    const String_impl_    *self = (const String_impl_*)s;
-    return (self->length);
+    const String_impl_    *this = (const String_impl_*)s;
+    return (this->length);
 }
 
 size_t  string_capacity(const String *s)
 {
     if (!s) return (-1);
-    const String_impl_    *self = (const String_impl_*)s;
-    return (self->capacity);
+    const String_impl_    *this = (const String_impl_*)s;
+    return (this->capacity);
 }
 
 int     string_reserve(String *s, size_t res_arg)
 {
     if (!s) return (-1);
-    String_impl_    *self = (String_impl_*)s;
-    if (self->capacity < res_arg) {
-        char    *tmp = realloc(self->data, res_arg);
+    String_impl_    *this = (String_impl_*)s;
+    if (this->capacity < res_arg) {
+        char    *tmp = realloc(this->data, res_arg);
         if (!tmp) return (-1);
-        self->data = tmp;
-        self->capacity = res_arg;
+        this->data = tmp;
+        this->capacity = res_arg;
     }
     return (0);
 }
@@ -117,142 +117,142 @@ int     string_reserve(String *s, size_t res_arg)
 void    string_clear(String *s)
 {
     if (!s) return ;
-    String_impl_    *self = (String_impl_*)s;
-    memset(self->data, 0, self->capacity);
-    self->length = 0;
+    String_impl_    *this = (String_impl_*)s;
+    memset(this->data, 0, this->capacity);
+    this->length = 0;
     return ;
 }
 
 bool    string_empty(const String *s)
 {
     if (!s) return (-1);
-    const String_impl_    *self = (const String_impl_*)s;
-    return (self->length == 0);
+    const String_impl_    *this = (const String_impl_*)s;
+    return (this->length == 0);
 }
 
 
 char    string_at(const String *s, size_t pos)
 {
     if (!s) return (0);
-    const String_impl_    *self = (const String_impl_*)s;
-    if (self->length <= pos)
+    const String_impl_    *this = (const String_impl_*)s;
+    if (this->length <= pos)
         return (0);
-    return (self->data[pos]);
+    return (this->data[pos]);
 }
 
 
 String  *string_append(String *s, const char *str)
 {
     if (!s || !str) return (NULL);
-    String_impl_    *self = (String_impl_*)s;
-    size_t  expected_cap = self->length + strlen(str) + 1;
-    if (self->capacity < expected_cap)
+    String_impl_    *this = (String_impl_*)s;
+    size_t  expected_cap = this->length + strlen(str) + 1;
+    if (this->capacity < expected_cap)
     {
         size_t  new_cap = grow_capacity_(expected_cap);
-        char    *tmp = realloc(self->data, new_cap);
+        char    *tmp = realloc(this->data, new_cap);
         if (!tmp) return (NULL);
-        self->data = tmp;
-        self->capacity = new_cap;
+        this->data = tmp;
+        this->capacity = new_cap;
     }
-    strcpy(self->data + self->length, str);
-    self->length += strlen(str);
+    strcpy(this->data + this->length, str);
+    this->length += strlen(str);
     return (s);
 }
 
 String  *string_insert(String *s, size_t pos, const char *str)
 {
     if (!s || !str) return (NULL);
-    String_impl_    *self = (String_impl_*)s;
-    if (pos > self->length) return (NULL);
+    String_impl_    *this = (String_impl_*)s;
+    if (pos > this->length) return (NULL);
 
     size_t  add = strlen(str);
-    size_t  need = self->length + add + 1;
-    if (self->capacity < need)
+    size_t  need = this->length + add + 1;
+    if (this->capacity < need)
     {
-        size_t  new_cap = grow_capacity_(self->length + add);
-        char    *tmp = realloc(self->data, new_cap);
+        size_t  new_cap = grow_capacity_(this->length + add);
+        char    *tmp = realloc(this->data, new_cap);
         if (!tmp) return (NULL);
-        self->data = tmp;
-        self->capacity = new_cap;
+        this->data = tmp;
+        this->capacity = new_cap;
     }
-    memmove(self->data + pos + add, self->data + pos, self->length - pos + 1);
-    memcpy(self->data + pos, str, add);
-    self->length += add;
+    memmove(this->data + pos + add, this->data + pos, this->length - pos + 1);
+    memcpy(this->data + pos, str, add);
+    this->length += add;
     return (s);
 }
 
 String  *string_erase(String *s, size_t pos, size_t n)
 {
     if (!s) return (NULL);
-    String_impl_    *self = (String_impl_*)s;
-    if (pos > self->length) return (NULL);
+    String_impl_    *this = (String_impl_*)s;
+    if (pos > this->length) return (NULL);
 
-    size_t  rest = self->length - pos;
+    size_t  rest = this->length - pos;
     if (n > rest) n = rest;
-    memmove(self->data + pos, self->data + pos + n, rest - n + 1);
-    self->length -= n;
+    memmove(this->data + pos, this->data + pos + n, rest - n + 1);
+    this->length -= n;
     return (s);
 }
 
 String  *string_replace(String *s, size_t pos, size_t n, const char *str)
 {
     if (!s || !str) return (NULL);
-    String_impl_    *self = (String_impl_*)s;
-    if (pos > self->length) return (NULL);
+    String_impl_    *this = (String_impl_*)s;
+    if (pos > this->length) return (NULL);
 
-    size_t  rest = self->length - pos;
+    size_t  rest = this->length - pos;
     if (n > rest) n = rest;
 
     size_t  add = strlen(str);
-    size_t  new_len = self->length - n + add;
-    if (self->capacity < new_len + 1)
+    size_t  new_len = this->length - n + add;
+    if (this->capacity < new_len + 1)
     {
         size_t  new_cap = grow_capacity_(new_len);
-        char    *tmp = realloc(self->data, new_cap);
+        char    *tmp = realloc(this->data, new_cap);
         if (!tmp) return (NULL);
-        self->data = tmp;
-        self->capacity = new_cap;
+        this->data = tmp;
+        this->capacity = new_cap;
     }
-    memmove(self->data + pos + add, self->data + pos + n, rest - n + 1);
-    memcpy(self->data + pos, str, add);
-    self->length = new_len;
+    memmove(this->data + pos + add, this->data + pos + n, rest - n + 1);
+    memcpy(this->data + pos, str, add);
+    this->length = new_len;
     return (s);
 }
 
 const char    *string_c_str(const String *s)
 {
     if (!s) return (NULL);
-    const String_impl_    *self = (const String_impl_*)s;
-    return (self->data);
+    const String_impl_    *this = (const String_impl_*)s;
+    return (this->data);
 }
 
 size_t  string_copy(const String *s, char *dst, size_t n, size_t pos)
 {
     if (!s || !dst) return (STRING_NPOS);
-    const String_impl_    *self = (const String_impl_*)s;
-    if (pos > self->length) return (STRING_NPOS);
-    size_t  rest = self->length - pos;
+    const String_impl_    *this = (const String_impl_*)s;
+    if (pos > this->length) return (STRING_NPOS);
+    size_t  rest = this->length - pos;
     if (n > rest) n = rest;
-    memcpy(dst, self->data + pos, n);
+    memcpy(dst, this->data + pos, n);
     return (n);
 }
 
 size_t  string_find(const String *s, const char *str)
 {
     if (!s || !str) return (STRING_NPOS);
-    const String_impl_    *self = (const String_impl_*)s;
-    const char  *hit = strstr(self->data, str);
+    const String_impl_    *this = (const String_impl_*)s;
+    const char  *hit = strstr(this->data, str);
     if (!hit) return (STRING_NPOS);
-    return ((size_t)(hit - self->data));
+    return ((size_t)(hit - this->data));
 }
 
 int     string_substr(String *d, const String *s, size_t pos, size_t n)
 {
     if (!d || !s) return (-1);
-    const String_impl_    *self = (const String_impl_*)s;
-    if (pos > self->length) return (-1);
+    const String_impl_    *this = (const String_impl_*)s;
+    if (pos > this->length) return (-1);
 
-    size_t  rest = self->length - pos;
+    size_t  rest = this->length - pos;
     if (n > rest) n = rest;
 
     String_impl_    *dst = (String_impl_*)d;
@@ -260,16 +260,16 @@ int     string_substr(String *d, const String *s, size_t pos, size_t n)
     dst->capacity = grow_capacity_(n);
     dst->data = calloc(dst->capacity, sizeof(char));
     if (!dst->data) return (-1);
-    memcpy(dst->data, self->data + pos, n);
+    memcpy(dst->data, this->data + pos, n);
     return (0);
 }
 
 int     string_compare(const String *s, const String *r)
 {
     if (!s || !r) return (-1);
-    const String_impl_    *self = (const String_impl_*)s;
+    const String_impl_    *this = (const String_impl_*)s;
     const String_impl_    *rhs = (const String_impl_*)r;
-    return (strcmp(self->data, rhs->data));
+    return (strcmp(this->data, rhs->data));
 }
 
 
@@ -329,8 +329,8 @@ bool    string_greater_equal(const String *l, const String *r)
 int     string_print(int fd, String *s)
 {
     if (!s) return (-1);
-    String_impl_    *self = (String_impl_*)s;
-    return (dprintf(fd, "%s", self->data));
+    String_impl_    *this = (String_impl_*)s;
+    return (dprintf(fd, "%s", this->data));
 }
 
 int     string_swap(String *l, String *r)
--- String.h
+++ String.h
@@ -13,48 +13,48 @@ typedef union String
 
 # define STRING_NPOS ((size_t)-1)
 
-int     string_construct(String *self, const char *str);
-void    string_destruct(String *self);
-int     string_copy_construct(String *self, const String *other);
+int     string_construct(String *this, const char *str);
+void    string_destruct(String *this);
+int     string_copy_construct(String *this, const String *other);
 String  *string_assign(String *lhs, const String *rhs);
 
-char    *string_begin(String *self);
-char    *string_end(String *self);
-// char    *string_rbegin(const String *self);
-// char    *string_rend(const String *self);
+char    *string_begin(String *this);
+char    *string_end(String *this);
+// char    *string_rbegin(const String *this);
+// char    *string_rend(const String *this);
 
-// size_t  string_size(const String *self);
-size_t  string_length(const String *self);
-// size_t  string_max_size(const String *self);
-// int     string_resize(String *self, size_t n, char pad);
-size_t  string_capacity(const String *self);
-int     string_reserve(String *self, size_t res_arg);
-void    string_clear(String *self);
-bool    string_empty(const String *self);
+// size_t  string_size(const String *this);
+size_t  string_length(const String *this);
+// size_t  string_max_size(const String *this);
+// int     string_resize(String *this, size_t n, char pad);
+size_t  string_capacity(const String *this);
+int     string_reserve(String *this, size_t res_arg);
+void    string_clear(String *this);
+bool    string_empty(const String *this);
 
-char    string_at(const String *self, size_t pos);
+char    string_at(const String *this, size_t pos);
 
-String  *string_append(String *self, const char *str);
-// int     string_push_back(String *self);
-String  *string_insert(String *self, size_t pos, const char *str);
-String  *string_erase(String *self, size_t pos, size_t n);
-String  *string_replace(String *self, size_t pos, size_t n, const char *str);
+String  *string_append(String *this, const char *str);
+// int     string_push_back(String *this);
+String  *string_insert(String *this, size_t pos, const char *str);
+String  *string_erase(String *this, size_t pos, size_t n);
+String  *string_replace(String *this, size_t pos, size_t n, const char *str);
 
-const char    *string_c_str(const String *self);
-// char    *string_data(const String *self);
+const char    *string_c_str(const String *this);
+// char    *string_data(const String *this);
 /* dst に最大 n 文字コピーする。終端 NUL は付けない (std::string::copy と同じ)。
    戻り値は実際にコピーした文字数、エラー時は STRING_NPOS */
-size_t  string_copy(const String *self, char *dst, size_t n, size_t pos);
-size_t  string_find(const String *self, const char *str);
-// size_t  string_rfind(const String *self, char *str);
-// size_t  string_find_first_of(const String *self, char c);
-// size_t  string_find_last_of(const String *self, char c);
-// size_t  string_find_first_not_of(const String *self, char c);
-// size_t  string_find_last_not_of(const String *self, char c);
+size_t  string_copy(const String *this, char *dst, size_t n, size_t pos);
+size_t  string_find(const String *this, const char *str);
+// size_t  string_rfind(const String *this, char *str);
+// size_t  string_find_first_of(const String *this, char c);
+// size_t  string_find_last_of(const String *this, char c);
+// size_t  string_find_first_not_of(const String *this, char c);
+// size_t  string_find_last_not_of(const String *this, char c);
 /* dst は未構築の String を渡すこと (構築済みだと元の data がリークする)。
    成功したら呼び出し側が string_destruct で解放する */
-int     string_substr(String *dst, const String *self, size_t pos, size_t n);
-int     string_compare(const String *self, const String *rhs);
+int     string_substr(String *dst, const String *this, size_t pos, size_t n);
+int     string_compare(const String *this, const String *rhs);
 
 int     string_join(String *dst, const String *lhs, const String *rhs);
 bool    string_equal(const String *lhs, const String *rhs);
@@ -64,8 +64,8 @@ bool    string_less_equal(const String *lhs, const String *rhs);
 bool    string_greater_than(const String *lhs, const String *rhs);
 bool    string_greater_equal(const String *lhs, const String *rhs);
 
-int     string_print(int fd, String *self);
-// int     string_read(int fd, String *self);
+int     string_print(int fd, String *this);
+// int     string_read(int fd, String *this);
 // int     string_getline(String *dst, int fd);
 
 int     string_swap(String *lhs, String *rhs);
